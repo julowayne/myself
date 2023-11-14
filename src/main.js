3f372ca3ca829fd48e1c473ce2548a622ceb81e9
Jules <thomas.desessarts-jules@hotmail.com>
2023-11-14 15:13:56 +0100

    added font awesome

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCircleExclamation)
+
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
